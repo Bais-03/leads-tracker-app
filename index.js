002c21f056c0380051666e84d19eb71428b6f17f
Bais-03 <baisrenukaa@gmail.com>
2025-06-21 15:08:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js"
 import { getDatabase,
          ref,
          push,
          onValue,
-         remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+         remove } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js"
 
 const firebaseConfig = {
-    databaseURL: "https://leads-tracker-app-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://leads-tracker-12f05-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(firebaseConfig)
